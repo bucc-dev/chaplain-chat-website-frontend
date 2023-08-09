04d60fe164d0d5a28b7b9036ccb5963e75177248
benhexie <benedictgabriel73@gmail.com>
2023-08-09 05:39:20 +0100

    Added active style to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,30 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => {
+  const location = useLocation().pathname;
+
   return (
     <div>
       <nav className="home-page__nav">
-        <Link to="/" className="home-page__nav__link">
+        <Link
+          to="/"
+          className={`home-page__nav__link ${location === "/" && "active"}`}
+        >
           Home
         </Link>
-        <Link to="/vent" className="home-page__nav__link">
+        <Link
+          to="/vent"
+          className={`home-page__nav__link ${location === "/vent" && "active"}`}
+        >
           Vent
         </Link>
-        <Link to="#" /* /contact */ className="home-page__nav__link">
+        <Link
+          to="#"
+          /* /contact */ className={`home-page__nav__link ${
+            location === "/contact" && "active"
+          }`}
+        >
           Contact
         </Link>
         <Link to="#" /* /communicate */ className="home-page__nav__link">
