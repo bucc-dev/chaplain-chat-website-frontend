efcd328abe9269e40142e7cb74a6b740bf7a0cd0
benhexie <benedictgabriel73@gmail.com>
2023-08-17 03:54:57 +0100

    Added confirmation overlay

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = ({ setScrollTap }) => {
--- src/pages/Home/components/ChatCard.jsx
+++ src/pages/Home/components/ChatCard.jsx
@@ -1,24 +1,36 @@
 import { Link } from "react-router-dom";
 import { Close } from "../../../assets";
 import "./ChatCard.css";
+import Submitted from "./Submittted";
+import { useState } from "react";
 
 const ChatCard = ({ desc, messagePlaceholder, emailPlaceholder }) => {
-  return (
-    <div className="chat__card">
-      <Link to={"/"} replace className="close__icon">
-        <Close />
-      </Link>
-      <h3>Welcome,</h3>
-      <p>{desc}</p>
-      <textarea placeholder={messagePlaceholder} />
-      {emailPlaceholder && (
-        <div className="input__container">
-          <input placeholder={emailPlaceholder} />
-          <button className="btn">Done</button>
-        </div>
-      )}
-    </div>
-  );
+
+  const [submitted, setSubmitted] = useState(false);
+  const sendResponse = () => {
+    setSubmitted(true);
+  }
+
+  if (!submitted) {
+    return (
+      <div className="chat__card">
+        <Link to={"/"} replace className="close__icon">
+          <Close />
+        </Link>
+        <h3>Welcome,</h3>
+        <p>{desc}</p>
+        <textarea placeholder={messagePlaceholder} />
+        {emailPlaceholder && (
+          <div className="input__container">
+            <input placeholder={emailPlaceholder} />
+            <button className="btn" onClick={sendResponse}>Done</button>
+          </div>
+        )}
+      </div>
+    )
+  } else {
+    return <Submitted />
+  }
 };
 
 export default ChatCard;
