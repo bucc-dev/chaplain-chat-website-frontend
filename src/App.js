1c6f7b65dae5132d76e05465e6ffaeaf862526fc
benhexie <benedictgabriel73@gmail.com>
2023-10-24 23:40:09 +0100

    Added fetch request to vent

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import FrontPage from "./pages/Home";
 import Home from "./pages/Home/Home";
 import CardView from "./pages/Home/overlays/CardView";
 import ErrorPage from "./pages/Error";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Router>
         <Routes>
           <Route path="/">
--- src/pages/Home/components/ChatCard.jsx
+++ src/pages/Home/components/ChatCard.jsx
@@ -2,31 +2,94 @@ import { Link } from "react-router-dom";
 import { Close } from "../../../assets";
 import "./ChatCard.css";
 import Submitted from "./Submittted";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const ChatCard = ({ desc, messagePlaceholder, emailPlaceholder }) => {
 
   const [submitted, setSubmitted] = useState(false);
-  const sendResponse = () => {
-    setSubmitted(true);
+  const [message, setMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setMessage("");
+    setEmail("");
+  }, []);
+
+  useEffect(() => {
+    setSubmitted(false);
+  }, [submitted])
+
+  const sendResponse = async (e) => {
+    e.preventDefault();
+    if (!message) return;
+    try {
+      setLoading(true);
+      const response = await fetch("https://chaplaincy-backend.onrender.com/api/vents", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({message, email})
+      })
+      const data = await response.json();
+      setLoading(false);
+      if (/success/i.test(data.status)) {
+        toast.success("Submission successful!")
+        setSubmitted(true);
+        return;
+      }
+      toast.error("Submission failed")
+    } catch (err) {
+      setLoading(false);
+      toast.error("Submission failed")
+    }
   }
 
   if (!submitted) {
     return (
-      <div className="chat__card">
+      <form className="chat__card" onSubmit={sendResponse}>
         <Link to={"/"} replace className="close__icon">
           <Close />
         </Link>
         <h3>Welcome,</h3>
         <p>{desc}</p>
-        <textarea placeholder={messagePlaceholder} />
-        {emailPlaceholder && (
-          <div className="input__container">
-            <input placeholder={emailPlaceholder} />
-            <button className="btn" onClick={sendResponse}>Done</button>
-          </div>
-        )}
-      </div>
+        <textarea 
+          required
+          autoFocus
+          value={message} 
+          onChange={e => setMessage(e.target.value)} 
+          placeholder={messagePlaceholder} />
+        {
+          emailPlaceholder ? (
+            <div className="input__container">
+              <input
+                required
+                value={email} 
+                type="email"
+                onChange={e => setEmail(e.target.value)}  
+                placeholder={emailPlaceholder} />
+              <button 
+                disabled={loading ? true : false} 
+                className="btn">
+                  {
+                    !loading ? "Done" : "Sending..."
+                  }
+              </button>
+            </div>
+          ) : (
+            <button 
+              disabled={loading ? true : false}
+              className="btn vent__btn">
+                {
+                  !loading ? "Done" : "Sending..."
+                }
+              </button>
+          )
+        }
+      </form>
     )
   } else {
     return <Submitted />
