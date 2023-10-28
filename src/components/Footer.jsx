d7d2fa94e7e887a06b09a4cf70a3d1f1f3496677
benhexie <benedictgabriel73@gmail.com>
2023-10-28 09:48:36 +0100

    Added contact links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="home-page__footer">
       <p>
         You can get in touch with the chaplain directly by emailing her at
-        estheromosehin05@gmail.com or calling her at 08103035840. (For important
+        <a href="mailto:estheromosehin05@gmail.com">estheromosehin05@gmail.com</a> or calling her at <a href="tel:+2348103035840">08103035840</a>. (For important
         reasons ONLY)
       </p>
     </footer>
