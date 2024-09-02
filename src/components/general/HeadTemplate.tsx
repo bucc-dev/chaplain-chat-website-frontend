15c7df502a3c131c1b1deec32c4f3ef2c4f7e2dd
Victor Ibironke <victoribironke2019@gmail.com>
2024-09-02 21:48:56 +0100

    fix: fixed bug with seo (title)

--- src/components/general/HeadTemplate.tsx
+++ src/components/general/HeadTemplate.tsx
@@ -6,8 +6,8 @@ const HeadTemplate = ({ title }: HeadTemplateProps) => {
   const meta = {
     title: title ? `${title} ~ BCW` : "BCW",
     description: "Anonymously chat with the chaplain.",
-    url: "https://www.babcock.tools",
-    image: "https://www.babcock.tools/images/logo.jpeg",
+    url: "",
+    image: "",
     logo: IMAGES.logo.src,
   };
 
@@ -20,14 +20,14 @@ const HeadTemplate = ({ title }: HeadTemplateProps) => {
       {/* <!-- Facebook Meta Tags --> */}
       <meta property="og:url" content={meta.url} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="BCW" />
+      <meta property="og:title" content={meta.title} />
       <meta property="og:description" content={meta.description} />
       <meta property="og:image" content={meta.image} />
       {/* <!-- Twitter Meta Tags --> */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content={meta.url.split("https://")[0]} />
       <meta property="twitter:url" content={meta.url} />
-      <meta name="twitter:title" content="BCW" />
+      <meta name="twitter:title" content={meta.title} />
       <meta name="twitter:description" content={meta.description} />
       <meta name="twitter:image" content={meta.image} />
     </Head>
